05e9ac048ed537a729dfa4eda9fb3b03210c375c
zteen <anders.steen98@gmail.com>
2018-04-18 17:30:45 +0200

    PlayerFollow yOffset and xOffset addition
    PlayerFollow now makes the camera offset in the x-direction the player is moving. The camera y-position is also clamped to never go below the ground.

--- Messenger Run/Assets/Scripts/FollowPlayer.cs
+++ Messenger Run/Assets/Scripts/FollowPlayer.cs
@@ -6,19 +6,42 @@ public class FollowPlayer : MonoBehaviour {
 
     public GameObject player;
 
-    private Vector3 zOffset = new Vector3(0, 0, 10);
+    private float zOffset = -10f;
+    private float yOffset = 5f;
+    private float xOffset = 0f;
+    private float maxXOffset = 2f;
 
-	// Use this for initialization
-	void Start() {
-
-	}
-	
-	// Update is called once per frame
-	void Update() {
+    // Update is called once per frame
+    void Update() {
         if (player != null) {
-            transform.position = player.transform.position - zOffset;
+            updateXOffset();
+            transform.position = new Vector3(player.transform.position.x + xOffset, Mathf.Clamp(player.transform.position.y, yOffset, Mathf.Infinity), zOffset);
         } else {
             player = GameObject.FindGameObjectWithTag("Player");
         }
-	}
+    }
+
+    // Checks direction of player movement, and changes xOffset accordingly. Returns updated value of xOffset
+    void updateXOffset() {
+        if (isMovingRight()) {
+            xOffset += 0.05f;
+        } else if (isMovingLeft()) {
+            xOffset -= 0.05f;
+        } else {
+            if (System.Math.Abs(xOffset) < 0.1) {
+                xOffset = 0f;
+            } else {
+                xOffset /= 1.2f;
+            }
+        }
+        xOffset = Mathf.Clamp(xOffset, -maxXOffset, maxXOffset);
+    }
+
+    bool isMovingRight() {
+        return ((Rigidbody2D)player.GetComponent(typeof(Rigidbody2D))).velocity.x > 0;
+    }
+
+    bool isMovingLeft() {
+        return ((Rigidbody2D)player.GetComponent(typeof(Rigidbody2D))).velocity.x < 0;
+    }
 }
